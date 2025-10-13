2a631cd5f16b76a866b4f73a5dbb4f00b9fcbcd5
QaldeK <qaldek@gmx.com>
2025-10-13 21:24:45 +0200

    svelte 0.1

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<main>
+  <div class="p-8">
+
+  </div>
+</main>
+
+<style>
+/* Pas de styles ici - on utilise Tailwind/DaisyUI */
+</style>
---
+++ svelte-app/vite.config.ts
@@ -0,0 +1,38 @@
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(), svelte()],
+  build: {
+      // Cible le dossier static/dist de votre thème Hugo
+      outDir: '../static/dist',
+      emptyOutDir: true, // Vide le dossier à chaque build (utile pour le dev)
+      // Pour des noms de fichiers propres
+      rollupOptions: {
+        output: {
+          entryFileNames: 'assets/[name].js',
+          chunkFileNames: 'assets/[name].js',
+          assetFileNames: 'assets/[name].[ext]'
+        }
+      }
+    },
+
+    // Important pour que les imports fonctionnent correctement
+    resolve: {
+      alias: {
+        '$lib': '/src/lib'
+      }
+    },
+
+    // --- Configuration du serveur de développement ---
+    server: {
+      // Très important : pour éviter une boucle infinie
+      // Vite ne doit pas "watch" les fichiers qu'il écrit lui-même.
+      // // En vrai c'est bon, c'est dans le parent ..?
+      // watch: {
+      //   ignored: ['**/static/dist/**']
+      // }
+    }
+})
